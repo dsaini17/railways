86f9be4b4ad13aff941aa080623033b16bf3de0a
Devesh Saini <devesh_saini@intuit.com>
2021-11-27 01:17:59 +0530

    draft

--- Railways/ViewController.swift
+++ Railways/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+   var webView: WKWebView!
+   override func viewDidLoad() {
+      super.viewDidLoad()
+    
+//    let preferences = WKWebPagePreferences()
+//    preferences.allowContentJavaScript = true
+//    let configuration = WKWebViewConfiguration()
+//    configuration.preferences = preferences
+//    webView = WKWebView(frame: view.bounds, configuration: configuration)
+//        view.addSubview(webView)
+    
+      NSLog("Attempting to open url")
+      let myURL = URL(string:"https://insiderailways.com/")
+      let myRequest = URLRequest(url: myURL!)
+    
+      // enable JS
+      // webView.configuration.preferences.javaScriptEnabled = true
+    
+      webView.load(myRequest)
+      NSLog("URL loaded")
+   }
+   override func loadView() {
+      let webConfiguration = WKWebViewConfiguration()
+      webView = WKWebView(frame: .zero, configuration: webConfiguration)
+      webView.uiDelegate = self
+      view = webView
+   }
 }
-
